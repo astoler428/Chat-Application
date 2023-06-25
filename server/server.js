92581f0156d154674facf9bbf5f549293e4d908a
Ari Stoler <aristoler@aris-air.lan>
2023-06-24 20:36:11 -0400

    trying to get build script right

--- server/server.js
+++ server/server.js
@@ -11,30 +11,21 @@ const cors = require("cors");
 const server = http.createServer(app);
 const connectDB = require("./config/dbConn");
 const bodyParser = require("body-parser");
-const {
-  login,
-  createNewUser,
-  getAllUsers,
-  deleteUser,
-  updatePassword,
-} = require("./controller/userController");
+const { login, createNewUser, getAllUsers, deleteUser, updatePassword, } = require("./controller/userController");
 const { addContact, getContacts } = require("./controller/contactController");
 const { getRoomID } = require("./controller/roomController");
-const {
-  storeMessage,
-  getMessageHistory,
-} = require("./controller/messageController");
+const { storeMessage, getMessageHistory, } = require("./controller/messageController");
 //variables to track the socket information
 const usernameSocketMap = new Map(); //map socket.id to username
 let currentRoomID = "";
 //connects to Database
 connectDB();
 exports.io = new socket_io_1.Server(server, {
-  cors: {
-    // origin: "http://localhost:5173",
-    origin: "https://ari-chatapp.netlify.app",
-    methods: ["GET", "POST", "PUT"],
-  },
+    cors: {
+        // origin: "http://localhost:5173",
+        origin: "http://ari-chatapp.netlify.app",
+        methods: ["GET", "POST", "PUT"],
+    },
 });
 app.use(cors());
 app.use(express.urlencoded({ extended: true }));
@@ -51,50 +42,48 @@ app.get("/messages/:id", getMessageHistory);
 app.post("/delete", deleteUser);
 app.put("/changepassword", updatePassword);
 exports.io.on("connection", (socket) => {
-  //called when a new user connects. Put socket.id and username into map
-  socket.on("store-username", (username) => {
-    usernameSocketMap.set(socket.id, username);
-  });
-  //called when a user joins a room
-  socket.on("join", (roomID) => {
-    socket.join(roomID);
-    currentRoomID = roomID;
-    notifyRoomMembers(roomID);
-  });
-  //called when a user joins a room
-  socket.on("leave", (roomID) => {
-    socket.leave(roomID);
-    currentRoomID = "";
-    notifyRoomMembers(roomID);
-  });
-  //called when a new message is sent. Gets stored in database and shared
-  socket.on("message", (message, roomID, name) => {
-    storeMessage(roomID, name, message);
-    socket.to(roomID).emit("message", message, name);
-  });
-  //notify room members that you left and remove id/username from map
-  socket.on("disconnect", () => {
-    if (currentRoomID !== "") notifyRoomMembers(currentRoomID);
-    usernameSocketMap.delete(socket.id);
-  });
-  function notifyRoomMembers(roomID) {
-    const roomMembers = getUsernamesInRoom(roomID);
-    socket.nsp.to(roomID).emit("room-members", roomMembers); //nsp sends to self apparently
-  }
+    //called when a new user connects. Put socket.id and username into map
+    socket.on("store-username", (username) => {
+        usernameSocketMap.set(socket.id, username);
+    });
+    //called when a user joins a room
+    socket.on("join", (roomID) => {
+        socket.join(roomID);
+        currentRoomID = roomID;
+        notifyRoomMembers(roomID);
+    });
+    //called when a user joins a room
+    socket.on("leave", (roomID) => {
+        socket.leave(roomID);
+        currentRoomID = "";
+        notifyRoomMembers(roomID);
+    });
+    //called when a new message is sent. Gets stored in database and shared
+    socket.on("message", (message, roomID, name) => {
+        storeMessage(roomID, name, message);
+        socket.to(roomID).emit("message", message, name);
+    });
+    //notify room members that you left and remove id/username from map
+    socket.on("disconnect", () => {
+        if (currentRoomID !== "")
+            notifyRoomMembers(currentRoomID);
+        usernameSocketMap.delete(socket.id);
+    });
+    function notifyRoomMembers(roomID) {
+        const roomMembers = getUsernamesInRoom(roomID);
+        socket.nsp.to(roomID).emit("room-members", roomMembers); //nsp sends to self apparently
+    }
 });
 //helper function that returns all usernames in a particular room
 function getUsernamesInRoom(roomID) {
-  var _a;
-  const roomMembers = [];
-  const rooms = exports.io.of("/").adapter.rooms;
-  (_a = rooms.get(roomID)) === null || _a === void 0
-    ? void 0
-    : _a.forEach((socketID) =>
-        roomMembers.push(usernameSocketMap.get(socketID))
-      );
-  return roomMembers;
+    var _a;
+    const roomMembers = [];
+    const rooms = exports.io.of("/").adapter.rooms;
+    (_a = rooms
+        .get(roomID)) === null || _a === void 0 ? void 0 : _a.forEach((socketID) => roomMembers.push(usernameSocketMap.get(socketID)));
+    return roomMembers;
 }
 mongoose.connection.once("open", () => {
-  console.log("Connected to MongoDB");
-  server.listen(3000, () => console.log("server listening on port 3000"));
+    console.log("Connected to MongoDB");
+    server.listen(3000, () => console.log("server listening on port 3000"));
 });
